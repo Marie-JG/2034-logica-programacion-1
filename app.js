ce0f4455963375d43f0e6aa66e73d7dce508d3e3
marie-jg <marielajorio@gmail.com>
2024-01-20 19:12:33 -0300

    modulo1

---
+++ app.js
@@ -0,0 +1,7 @@
+let numeroSecreto= 6;
+let numeroUsuario= prompt ("Me indicas un número por favor: ");
+console.log (numeroUsuario);
+
+if(numeroUsuario == numeroSecreto){
+    alert ('Acertaste el número');
+}
