7a9cb22f3b31a674b72cea35a409b33a71070239
marie-jg <marielajorio@gmail.com>
2024-01-20 22:03:55 -0300

    modulo4

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 //Variables
-let numeroSecreto = 4;
+let numeroSecreto = Math.floor(Math.random()*10)+1;
 let numeroUsuario = 0;
 let intentos = 1;
-let palabraVeces = 'vez';
+//let palabraVeces = 'vez';
+let maximosIntentos = 3;
+
 
 while (numeroUsuario != numeroSecreto) {
-    numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
+    numeroUsuario = parseInt(prompt("Me indicas un número entre 1 y 10 por favor:"));
 
-    console.log(numeroUsuario);
+    console.log(typeof(numeroUsuario));
     if (numeroUsuario == numeroSecreto) {
         //Acertamos, fue verdadera la condición
-        alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${palabraVeces}`);
+        alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces' }`);
     } else {
         if (numeroUsuario > numeroSecreto) {
             alert('El número secreto es menor');
@@ -18,52 +20,16 @@ while (numeroUsuario != numeroSecreto) {
             alert('El número secreto es mayor');
         }
         //Incrementamos el contador cuando no acierta
-        intentos = intentos + 1;
-        palabraVeces = 'veces';
+        //intentos = intentos + 1;
+        //intentos += 1;
+        intentos++;
+
+        //palabraVeces = 'veces';
+        if (intentos > maximosIntentos) {
+            alert(`Llegaste al número máximo de ${maximosIntentos} intentos`);
+            break;
+        }
         //La condición no se cumplió
         //alert('Lo siento, no acertaste el número');
     }
 }
-
-/*
-Desafíos:
-
-1-Crea un contador que comience en 1 y vaya hasta 10 usando un bucle 'while'. Muestra cada número.
-
-let contador = 1;
-while (contador <= 10) {
-console.log(contador);
-contador++;
-}
-
-2-Crea un contador que comience en 10 y vaya hasta 0 usando un bucle 'while'. Muestra cada número.
-
-let contador = 10;
-while (contador >= 0) {
-console.log(contador);
-contador--;
-}
-
-3-Crea un programa de cuenta progresiva. Pide un número y cuenta desde 0 hasta ese número utilizando
- un bucle 'while' en la consola del navegador.
-
- let numeroMaximo = prompt("Ingresa un número para la cuenta regresiva:");
-
-while (numeroMaximo >= 0) {
-console.log(numeroMaximo);
-numeroMaximo--;
-}
-
-
-4-Crea un programa de cuenta progresiva. Pide un número y cuenta desde 0 hasta ese número utilizando 
-un bucle 'while' en la consola del navegador.
-
-let numeroMaximo = prompt("Ingresa un número para la cuenta progresiva:");;
-let contador = 0;
-
-while (contador <= numeroMaximo) {
-console.log(contador);
-contador++
-}"
-
-*/
