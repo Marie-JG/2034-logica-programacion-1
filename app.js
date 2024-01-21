89be7c23affaaefce1c8c536e8adb90ba7d4e2a4
marie-jg <marielajorio@gmail.com>
2024-01-20 21:38:40 -0300

    modulo3

--- app.js
+++ app.js
@@ -1,69 +1,69 @@
-
 //Variables
-
-
-alert('Bienvenido al juego del número secreto');
-
-// Agrega un console.log para verificar el valor de "intento" después de la entrada del usuario
-let intento = prompt('Elige un número entre 1 y 10');
-console.log('Valor de intento:', intento);
-
 let numeroSecreto = 4;
+let numeroUsuario = 0;
+let intentos = 1;
+let palabraVeces = 'vez';
 
-// Agrega un console.log para verificar la comparación entre "intento" y "numeroSecreto"
-console.log('Resultado de la comparación:', intento == numeroSecreto);
+while (numeroUsuario != numeroSecreto) {
+    numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
 
-if (intento == numeroSecreto) {
-    alert('Adivinaste');
-} else {
-    // Agrega un console.log para verificar el valor de "numeroSecreto" cuando el jugador se equivoca
-    console.log('Valor del número secreto:', numeroSecreto);
-    alert('Perdiste! El número secreto era ' + numeroSecreto);
-    /* Es importante recordar que cuando el proyecto esté disponible para todas las personas,
-     en lo que llamamos en el contexto del software "entorno de producción", 
-     los comandos console.log deben eliminarse o desactivarse, ya que pueden causar problemas de rendimiento y seguridad.
-    */
+    console.log(numeroUsuario);
+    if (numeroUsuario == numeroSecreto) {
+        //Acertamos, fue verdadera la condición
+        alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${palabraVeces}`);
+    } else {
+        if (numeroUsuario > numeroSecreto) {
+            alert('El número secreto es menor');
+        } else {
+            alert('El número secreto es mayor');
+        }
+        //Incrementamos el contador cuando no acierta
+        intentos = intentos + 1;
+        palabraVeces = 'veces';
+        //La condición no se cumplió
+        //alert('Lo siento, no acertaste el número');
+    }
+}
+
+/*
+Desafíos:
+
+1-Crea un contador que comience en 1 y vaya hasta 10 usando un bucle 'while'. Muestra cada número.
+
+let contador = 1;
+while (contador <= 10) {
+console.log(contador);
+contador++;
+}
+
+2-Crea un contador que comience en 10 y vaya hasta 0 usando un bucle 'while'. Muestra cada número.
+
+let contador = 10;
+while (contador >= 0) {
+console.log(contador);
+contador--;
+}
+
+3-Crea un programa de cuenta progresiva. Pide un número y cuenta desde 0 hasta ese número utilizando
+ un bucle 'while' en la consola del navegador.
+
+ let numeroMaximo = prompt("Ingresa un número para la cuenta regresiva:");
+
+while (numeroMaximo >= 0) {
+console.log(numeroMaximo);
+numeroMaximo--;
 }
 
 
-/* Desafio: 
-1-Pregunta al usuario qué día de la semana es. Si la respuesta es "Sábado" o "Domingo", muestra "¡Buen fin de semana!".
- De lo contrario, muestra "¡Buena semana!".
+4-Crea un programa de cuenta progresiva. Pide un número y cuenta desde 0 hasta ese número utilizando 
+un bucle 'while' en la consola del navegador.
 
- let diaDeLaSemana= prompt ('Ingresa el día de la semana: (LU-MA-MI-JU-VI-SA-DO)');
- if(diaDeLaSemana==="SA"|| diaDeLaSemana==="DO"){
-    alert ("¡Buen fin de semana!");
- }else{
-    alert("Buena semana");
- }
- 
-2-Verifica si un número ingresado por el usuario es positivo o negativo. Muestra una alerta informativa.
-let numero = prompt('Escribe un número positivo o negativo');
-if (numero > 0) {
-    alert('Número positivo');
-} else if (numero < 0) {
-    alert('Número negativo');
-} else {
-    alert('El número es cero');
-}
+let numeroMaximo = prompt("Ingresa un número para la cuenta progresiva:");;
+let contador = 0;
 
-3-Crea un sistema de puntuación para un juego. Si la puntuación es mayor o igual a 100, muestra "¡Felicidades, has ganado!".
- En caso contrario, muestra "Intenta nuevamente para ganar.".
- let puntuacion = prompt('Ingrese su puntuación');
-if (puntuacion >= 100) {
-    alert('¡Felicidades, has ganado!');
-} else {
-    alert('Intenta nuevamente para ganar.');
-}
+while (contador <= numeroMaximo) {
+console.log(contador);
+contador++
+}"
 
-4-Crea un mensaje que informe al usuario sobre el saldo de su cuenta, utilizando un template string para incluir el valor del saldo.
-
-let saldoCuenta = 500; // Ejemplo de saldo
-
-alert(`Tu saldo es de ${saldoCuenta}.`);
-
-5-Pide al usuario que ingrese su nombre mediante un prompt. Luego, muestra una alerta de bienvenida usando ese nombre.
-
-let nombre = prompt('Por favor, ingresa tu nombre');
-alert(`¡Bienvenid@, ${nombre}!`);
 */
